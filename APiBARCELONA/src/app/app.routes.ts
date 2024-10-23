575d7f319d5b8b3906eb10aa00060d3b0c43e655
Pierre <pierredanjonrgmail.com>
2024-10-23 17:17:53 +0200

    definicion of the route for component paisList

--- APiBARCELONA/src/app/app.routes.ts
+++ APiBARCELONA/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PaisListComponent } from './components/pais-list/pais-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pais-list', component: PaisListComponent }
+];
